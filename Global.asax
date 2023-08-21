4cb9abb1333dc4c88f88b20b236b7fbc7b89c596
SinhaBindu <bindukumari97@yahoo.com>
2023-08-21 10:57:48 +0530

    Initial file committed

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="UmangMicro.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UmangMicro.Startup))]
+namespace UmangMicro
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
