d831f8ee25c71233d1086601090c9986c633cfc9
SinhaBindu <sinhabinduk@gmail.com>
2023-11-01 12:08:30 +0530

    Commityed

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -16,6 +16,7 @@ namespace UmangMicro
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
+                //defaults: new { controller = "Account", action = "Login", id = UrlParameter.Optional }
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
